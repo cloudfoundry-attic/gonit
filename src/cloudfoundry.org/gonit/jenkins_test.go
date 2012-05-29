6c95f17285d830f9bfd3eb76485f5351eb3d0e1e
Ben Lisbakken <lisbakke@gmail.com>
2012-05-29 16:46:35 -0700

    Testing jenkins.
    Change-Id: I3a808c830c3ca16aa3c1f8d4c924976e58061df6

--- src/cloudfoundry.org/gonit/jenkins_test.go
+++ src/cloudfoundry.org/gonit/jenkins_test.go
@@ -3,7 +3,13 @@ package jenkinstest
 import "testing"
 
 func TestJenkins(t *testing.T) {
-	if 1 != 1 {
+	if 1 != 2 {
 		t.Errorf("1 isn't equal to 1!")
+  }
+}
+
+func TestJenkins2(t *testing.T) {
+	if 1 != 2 {
+		t.Errorf("1 isn't equal to 2!")
   }
 }
