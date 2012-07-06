718f5e6bdd8aa31d1bfb53e0ca1a926df679d9da
Doug MacEachern <dougm@vmware.com>
2012-07-06 16:34:35 -0700

    Wrapper for RPC server configuration and lifecycle
    Currently supporting json RPC over unix or tcp socket
    
    Change-Id: If0022e5e2d6a6982ccee548d7040a7175feaf49b

---
+++ rpc.go
@@ -0,0 +1,81 @@
+// Copyright (c) 2012 VMware, Inc.
+
+// Wrapper for RPC server configuration and lifecycle
+
+package gonit
+
+import (
+	"fmt"
+	"net"
+	"net/rpc/jsonrpc"
+	"net/url"
+	"os"
+)
+
+type RpcServer struct {
+	listener net.Listener
+	cleanup  func()
+}
+
+// Construct a new RpcServer via string URL
+// Currently supporting json RPC over unix or tcp socket
+func NewRpcServer(listenURL string) (*RpcServer, error) {
+	var listener net.Listener
+	var cleanup func()
+
+	url, err := url.Parse(listenURL)
+	if err != nil {
+		return nil, err
+	}
+
+	switch url.Scheme {
+	case "tcp":
+		if url.Host == "" {
+			err = fmt.Errorf("Invalid URL %q", listenURL)
+		} else {
+			listener, err = net.Listen("tcp", url.Host)
+		}
+	case "", "unix":
+		listener, err = net.Listen("unix", url.Path)
+		cleanup = func() { os.Remove(url.Path) }
+	default:
+		err = net.UnknownNetworkError(url.Scheme)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	server := &RpcServer{
+		listener: listener,
+		cleanup:  cleanup,
+	}
+
+	return server, nil
+}
+
+// Close listener socket
+// In the case of unix socket, remove socket file.
+func (s *RpcServer) Shutdown() {
+	s.listener.Close()
+	if s.cleanup != nil {
+		s.cleanup()
+	}
+}
+
+// Accept unix|tcp connections and serve json RPCs
+func (s *RpcServer) Serve() error {
+	defer s.Shutdown()
+
+	for {
+		conn, err := s.listener.Accept()
+
+		if err != nil {
+			return err
+		}
+
+		go jsonrpc.ServeConn(conn)
+	}
+
+	panic("not reached")
+}
