6b0274fc7a567e5a3e95126eb6f61ffe929fd9c2
Ben Lisbakken <lisbakke@gmail.com>
2012-07-21 12:36:14 -0700

    Sys mem stats and some cleanup.
    - Made one instance of resourceManager that everyone should use.
    - Stopped passing interval to GetResource because that was stupid
    - Changed getMemUsed function to be getMem function that takes the kind of mem
    --- getMem(<pid>, "used") would get the used mem for a pid
    - Created the same type of function for sys mem
    - Made ResourceHolders hold DataTimestamps so that all data entries have
          timestamps
    
    Change-Id: I841d02d991d6bccf3b270b0c401958916fbfe792

--- resourcemanager.go
+++ resourcemanager.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/cloudfoundry/gosigar"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,40 +18,71 @@ import (
 //       the right thing to do is, especially when we have different rules
 //       requesting the same resourceXpid at different intervals.
 // - Do we really need to allow for any other rule data than uint64?
+// - Implement all sys stuff
 
 // This interface allows us to mock sigar in unit tests.
 type SigarInterface interface {
-	getMemResident(int) (uint64, error)
-	getProcUsedTimestamp(int) (*ProcUsedTimestamp, error)
+	getMem(pid int, kind string) (uint64, error)
+	getProcTime(pid int) (float64, error)
+	getSysMem(kind string) (uint64, error)
 }
-
-var sigarInterface SigarInterface = &SigarGetter{}
 
 type SigarGetter struct{}
 
 // Gets the Resident memory of a process.
-func (s *SigarGetter) getMemResident(pid int) (uint64, error) {
+func (s *SigarGetter) getMem(pid int, kind string) (uint64, error) {
 	mem := sigar.ProcMem{}
 	if err := mem.Get(pid); err != nil {
 		return 0, fmt.Errorf("Couldnt get mem for pid '%v'.", pid)
 	}
-	return mem.Resident, nil
+	switch kind {
+	case "size":
+		return mem.Size, nil
+	case "used":
+		return mem.Resident, nil
+	case "share":
+		return mem.Share, nil
+	case "minor_faults":
+		return mem.MinorFaults, nil
+	case "major_faults":
+		return mem.MajorFaults, nil
+	case "page_faults":
+		return mem.PageFaults, nil
+	}
+	return 0, fmt.Errorf("Unknown resource 'memory_%v'.", kind)
 }
 
-// Gets the proc time and a timestamp and returns a ProcUsedTimestamp.
-func (s *SigarGetter) getProcUsedTimestamp(pid int) (*ProcUsedTimestamp,
-	error) {
+// Gets the proc time and a timestamp and returns a DataTimestamp.
+func (s *SigarGetter) getProcTime(pid int) (float64, error) {
 	procTime := sigar.ProcTime{}
 	if err := procTime.Get(pid); err != nil {
-		return &ProcUsedTimestamp{},
-			fmt.Errorf("Couldnt get proctime for pid '%v'.", pid)
+		return 0, fmt.Errorf("Couldnt get proctime for pid '%v'.", pid)
 	}
-	return &ProcUsedTimestamp{
-		procUsed:      float64(procTime.Total),
-		nanoTimestamp: float64(time.Now().UnixNano()),
-	}, nil
+	return float64(procTime.Total), nil
 }
 
+func (s *SigarGetter) getSysMem(kind string) (uint64, error) {
+	mem := sigar.Mem{}
+	if err := mem.Get(); err != nil {
+		return 0, fmt.Errorf("Couldn't get %v sys mem: 'sys_memory_%v'.", kind,
+			err.Error())
+	}
+	switch kind {
+	case "total":
+		return mem.Total, nil
+	case "used":
+		return mem.Used, nil
+	case "free":
+		return mem.Free, nil
+	case "actual_free":
+		return mem.ActualFree, nil
+	case "actual_used":
+		return mem.ActualUsed, nil
+	}
+	return 0, fmt.Errorf("Unknown resource 'sys_memory_%v'.", kind)
+}
+
+// Don't create more.
 type ResourceManager struct {
 	resourceHolders         []*ResourceHolder
 	sigarInterface          SigarInterface
@@ -61,37 +93,48 @@ type ResourceHolder struct {
 	resourceName string
 	// TODO Changed to fixed size array when implementing global
 	// interval/duration.
-	data         []interface{}
+	dataTimestamps []DataTimestamp
 }
 
-type ProcUsedTimestamp struct {
-	procUsed      float64
+var resourceManager ResourceManager = ResourceManager{
+	sigarInterface: &SigarGetter{},
+}
+
+type DataTimestamp struct {
+	data          interface{}
 	nanoTimestamp float64
 }
 
 const FIVE_MIN_SECS = 300
 const NANO_TO_MILLI = float64(time.Millisecond)
 
-const MEMORY_USED_NAME = "memory_used"
-const CPU_PERCENT_NAME = "cpu_percent"
+const (
+	CPU_PERCENT_NAME = "cpu_percent"
+	MEMORY_PREFIX = "memory_"
+	MEMORY_USED_NAME = "memory_used"
+	SYS_MEMORY_PREFIX = "sys_memory_"
+	SYS_MEMORY_USED_NAME = "sys_memory_used"
+)
 
 var validResourceNames = map[string]bool{
 	MEMORY_USED_NAME: true,
 	CPU_PERCENT_NAME: true,
 }
 
-// Given an array of data which is of type ProcUsedTimestamp, will return the
+// Given an array of data which is of type DataTimestamp, will return the
 // percent of proc time that was used.
 func (r *ResourceHolder) calculateProcPercent() float64 {
-	data := r.data
+	data := r.dataTimestamps
 	if len(data) <= 1 {
 		return 0
 	}
-	last := data[len(data)-1].(ProcUsedTimestamp)
-	secondLast := data[len(data)-2].(ProcUsedTimestamp)
+	last := data[len(data)-1]
+	secondLast := data[len(data)-2]
+	lastProcTime := last.data.(float64)
+	secondLastProcTime := secondLast.data.(float64)
 	lastMilli := last.nanoTimestamp / NANO_TO_MILLI
 	secondLastMilli := secondLast.nanoTimestamp / NANO_TO_MILLI
-	return (last.procUsed - secondLast.procUsed) / (lastMilli - secondLastMilli)
+	return (lastProcTime - secondLastProcTime) / (lastMilli - secondLastMilli)
 }
 
 // Given a ParsedEvent, will populate the correct resourceHolder with the
@@ -100,11 +143,10 @@ func (r *ResourceManager) gatherResource(parsedEvent *ParsedEvent,
 	pid int) error {
 	processName := parsedEvent.processName
 	resourceName := parsedEvent.resourceName
-	interval := parsedEvent.interval
 	for _, resourceHolder := range r.resourceHolders {
 		if resourceHolder.processName == processName &&
 			resourceHolder.resourceName == resourceName {
-			if err := resourceHolder.gather(pid, interval); err != nil {
+			if err := resourceHolder.gather(pid, r.sigarInterface); err != nil {
 				return err
 			}
 			return nil
@@ -114,7 +156,7 @@ func (r *ResourceManager) gatherResource(parsedEvent *ParsedEvent,
 		processName:  processName,
 		resourceName: resourceName,
 	}
-	if err := resourceHolder.gather(pid, interval); err != nil {
+	if err := resourceHolder.gather(pid, r.sigarInterface); err != nil {
 		return err
 	}
 	r.resourceHolders = append(r.resourceHolders, &resourceHolder)
@@ -131,13 +173,13 @@ func (r *ResourceManager) GetResource(parsedEvent *ParsedEvent,
 		return nil, err
 	}
 	for _, resourceHolder := range r.resourceHolders {
-		lenResourceData := len(resourceHolder.data)
+		lenResourceData := len(resourceHolder.dataTimestamps)
 		if resourceHolder.processName == processName &&
 			resourceHolder.resourceName == resourceName && lenResourceData > 0 {
 			if resourceName == CPU_PERCENT_NAME {
 				return resourceHolder.calculateProcPercent(), nil
 			} else if resourceName == MEMORY_USED_NAME {
-				return resourceHolder.data[lenResourceData-1], nil
+				return resourceHolder.dataTimestamps[lenResourceData-1].data, nil
 			}
 		}
 	}
@@ -147,8 +189,9 @@ func (r *ResourceManager) GetResource(parsedEvent *ParsedEvent,
 }
 
 // A utility function that parses a rule's amount string and returns the value
-// as the correct type.  For instance, in a rule such as 'memory_used > 14mb' the
-// amount is '14mb' which this function would turn into a uint64 of 14*1024*1024.
+// as the correct type.  For instance, in a rule such as 'memory_used > 14mb'
+// the amount is '14mb' which this function would turn into a uint64 of
+// 14*1024*1024.
 func (r *ResourceManager) ParseAmount(resourceName string,
 	amount string) (interface{}, error) {
 	if resourceName == MEMORY_USED_NAME {
@@ -182,34 +225,43 @@ func (r *ResourceManager) ParseAmount(resourceName string,
 }
 
 // Saves a data point into the data array of the ResourceHolder.
-func (r *ResourceHolder) saveData(dataToSave interface{},
-	intervalAmount time.Duration) {
-	numDataToKeep := int(FIVE_MIN_SECS / intervalAmount.Seconds())
-	if len(r.data) >= numDataToKeep {
+func (r *ResourceHolder) saveData(dataToSave interface{}) {
+	dataTimestamp := DataTimestamp{
+		data:          dataToSave,
+		nanoTimestamp: float64(time.Now().UnixNano()),
+	}
+	numDataToKeep := 300
+	if len(r.dataTimestamps) >= numDataToKeep {
 		// Clear out old data.
-		amountToClear := len(r.data) - numDataToKeep
-		r.data = append(r.data[amountToClear:], dataToSave)
+		amountToClear := len(r.dataTimestamps) - numDataToKeep
+		r.dataTimestamps = append(r.dataTimestamps[amountToClear:], dataTimestamp)
 	} else {
-		r.data = append(r.data, dataToSave)
+		r.dataTimestamps = append(r.dataTimestamps, dataTimestamp)
 	}
 }
 
-// Gets the data for a resource and saves it to the ResourceHolder.  The
-// intervalAmount is used to know how much data to store and how much to kick
-// out.
-func (r *ResourceHolder) gather(pid int, intervalAmount time.Duration) error {
-	if r.resourceName == MEMORY_USED_NAME {
-		memResident, err := sigarInterface.getMemResident(pid)
+// Gets the data for a resource and saves it to the ResourceHolder.
+func (r *ResourceHolder) gather(pid int, sigarInterface SigarInterface) error {
+	if strings.HasPrefix(r.resourceName, MEMORY_PREFIX) {
+		memType := r.resourceName[len(MEMORY_PREFIX):]
+		mem, err := sigarInterface.getMem(pid, memType)
 		if err != nil {
 			return err
 		}
-		r.saveData(memResident, intervalAmount)
+		r.saveData(mem)
 	} else if r.resourceName == CPU_PERCENT_NAME {
-		procUsedTimestamp, err := sigarInterface.getProcUsedTimestamp(pid)
+		procTime, err := sigarInterface.getProcTime(pid)
 		if err != nil {
 			return err
 		}
-		r.saveData(*procUsedTimestamp, intervalAmount)
+		r.saveData(procTime)
+	} else if strings.HasPrefix(r.resourceName, SYS_MEMORY_PREFIX) {
+		memType := r.resourceName[len(SYS_MEMORY_PREFIX):]
+		sysMem, err := sigarInterface.getSysMem(memType)
+		if err != nil {
+			return err
+		}
+		r.saveData(sysMem)
 	}
 	return nil
 }
@@ -224,5 +276,5 @@ func (r *ResourceManager) IsValidResourceName(resourceName string) bool {
 
 // Allows the sigar interface to be set so that tests can set it.
 func (r *ResourceManager) SetSigarInterface(sigar SigarInterface) {
-	sigarInterface = sigar
+	r.sigarInterface = sigar
 }
