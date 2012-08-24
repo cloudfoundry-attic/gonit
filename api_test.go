9003452163d0e90e41d9d18b24215ee2fac4d41b
Doug MacEachern <dougm@vmware.com>
2012-08-23 21:06:51 -0700

    Implement RPC status and summary methods
    Includes cli output formatting
    
    Change-Id: I617289a5faf9655e2835a5fc351f8b258d7a51f1

--- api_test.go
+++ api_test.go
@@ -3,18 +3,23 @@
 package gonit_test
 
 import (
+	"bytes"
 	"github.com/bmizerany/assert"
 	. "github.com/cloudfoundry/gonit"
 	"github.com/cloudfoundry/gonit/test/helper"
+	"io/ioutil"
+	"math"
 	"net/rpc"
+	"os"
 	"strings"
 	"testing"
 )
 
 var rpcName = "TestAPI"
+var config = &ConfigManager{}
 
 func init() {
-	rpc.RegisterName(rpcName, NewAPI(nil))
+	rpc.RegisterName(rpcName, NewAPI(config))
 }
 
 func isActionError(err error) bool {
@@ -62,6 +67,126 @@ func TestAbout(t *testing.T) {
 		assert.Equal(t, nil, err)
 
 		assert.Equal(t, VERSION, gabout.Version)
+	})
+
+	assert.Equal(t, nil, err)
+}
+
+func tmpPidFile(t *testing.T, pid int) string {
+	file, err := ioutil.TempFile("", "api_test")
+	if err != nil {
+		t.Error(err)
+	}
+	if err := WritePidFile(pid, file.Name()); err != nil {
+		t.Fatal(err)
+	}
+	return file.Name()
+}
+
+// simple exercise of CliFormatters
+func testCliPrint(t *testing.T, value CliFormatter) {
+	buf := new(bytes.Buffer)
+	value.Print(buf)
+	assert.NotEqual(t, 0, buf.Len())
+}
+
+func TestStatus(t *testing.T) {
+	nprocesses := 0
+
+	type testProcess struct {
+		name    string
+		pid     int
+		ppid    int
+		running bool
+	}
+
+	// use pid/ppid of the go test process to test
+	// Running, sigar.ProcState, etc.
+	pid := os.Getpid()
+	ppid := os.Getppid()
+
+	groups := []struct {
+		name      string
+		processes []testProcess
+	}{
+		{
+			name: "a_group",
+			processes: []testProcess{
+				{"a_process", pid, ppid, true},
+				{"x_process", math.MaxInt32, -1, false},
+			},
+		},
+		{
+			name: "b_group",
+			processes: []testProcess{
+				{"b_process", pid, ppid, true},
+			},
+		},
+	}
+
+	for _, group := range groups {
+		for _, process := range group.processes {
+			// write pidfile for use by process.IsRunning()
+			pidfile := tmpPidFile(t, process.pid)
+			defer os.Remove(pidfile)
+
+			config.AddProcess(group.name, &Process{
+				Name:    process.name,
+				Pidfile: pidfile,
+			})
+
+			nprocesses++
+		}
+	}
+
+	err := helper.WithRpcServer(func(c *rpc.Client) {
+		statusGroup := rpcName + ".StatusGroup"
+		statusProcess := rpcName + ".StatusProcess"
+
+		// should get an error if group does not exist
+		err := c.Call(statusGroup, "enogroup", &ProcessGroupStatus{})
+		assert.NotEqual(t, nil, err)
+
+		// should get an error if process does not exist
+		err = c.Call(statusProcess, "enoprocess", &ProcessStatus{})
+		assert.NotEqual(t, nil, err)
+
+		for _, group := range groups {
+			gstat := &ProcessGroupStatus{}
+			err := c.Call(statusGroup, group.name, gstat)
+			assert.Equal(t, nil, err)
+			assert.Equal(t, len(group.processes), len(gstat.Group))
+			testCliPrint(t, gstat)
+
+			for _, process := range group.processes {
+				stat := &ProcessStatus{}
+				err := c.Call(statusProcess, process.name, stat)
+				assert.Equal(t, nil, err)
+
+				running := stat.Summary.Running
+				assert.Equal(t, process.running, running)
+				testCliPrint(t, stat)
+
+				if !running {
+					continue
+				}
+
+				assert.Equal(t, process.pid, stat.Pid)
+				assert.Equal(t, process.ppid, stat.State.Ppid)
+			}
+		}
+
+		all := &ProcessGroupStatus{}
+		err = c.Call(rpcName+".StatusAll", "", all)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, nprocesses, len(all.Group))
+		testCliPrint(t, all)
+
+		summary := &Summary{}
+		err = c.Call(rpcName+".Summary", "", summary)
+		assert.Equal(t, nil, err)
+		assert.Equal(t, nprocesses, len(summary.Processes))
+		testCliPrint(t, summary)
 	})
 
 	assert.Equal(t, nil, err)
