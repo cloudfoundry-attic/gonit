29ffffbb42a76e66bb1df865b5a454dd32093ea6
Ben Lisbakken <lisbakke@gmail.com>
2012-08-28 15:10:14 -0700

    Adding duration support to eventmanager.
    Also turn all resources to uint64.
    
    Change-Id: I229d526a57c16a9c6660171670b595ea2143caf1

--- resourcemanager.go
+++ resourcemanager.go
@@ -5,23 +5,17 @@ package gonit
 import (
 	"fmt"
 	"github.com/cloudfoundry/gosigar"
+	"math"
 	"strconv"
 	"time"
 )
 
-// TODO:
-// - Cache resources so that we don't ask for the resource 2x for 2 rules using
-//       it.
-// - Start using durations to calculate resource avg over the time duration.
-// - For right now we keep 5min of data for each resourceXpid.  Figure out what
-//       the right thing to do is, especially when we have different rules
-//       requesting the same resourceXpid at different intervals
-// - Do we really need to allow for any other rule data than uint64?
+// Bug(lisbakke): figure out how we change maxdatatostore when two rules are using the same resource, because they'll both be using the same resourceholder.
 
 // This interface allows us to mock sigar in unit tests.
 type SigarInterface interface {
 	getMemResident(pid int) (uint64, error)
-	getProcTime(pid int) (float64, error)
+	getProcTime(pid int) (uint64, error)
 }
 
 type SigarGetter struct{}
@@ -36,12 +30,12 @@ func (s *SigarGetter) getMemResident(pid int) (uint64, error) {
 }
 
 // Gets the proc time and a timestamp and returns a DataTimestamp.
-func (s *SigarGetter) getProcTime(pid int) (float64, error) {
+func (s *SigarGetter) getProcTime(pid int) (uint64, error) {
 	procTime := sigar.ProcTime{}
 	if err := procTime.Get(pid); err != nil {
 		return 0, fmt.Errorf("Couldnt get proctime for pid '%v'.", pid)
 	}
-	return float64(procTime.Total), nil
+	return procTime.Total, nil
 }
 
 // Don't create more.
@@ -53,8 +47,9 @@ type ResourceManager struct {
 type ResourceHolder struct {
 	processName     string
 	resourceName    string
-	dataTimestamps  []DataTimestamp
-	firstEntryIndex int
+	dataTimestamps  []*DataTimestamp
+	firstEntryIndex int64
+	maxDataToStore  int64
 }
 
 var resourceManager ResourceManager = ResourceManager{
@@ -62,14 +57,14 @@ var resourceManager ResourceManager = ResourceManager{
 }
 
 type DataTimestamp struct {
-	data          interface{}
-	nanoTimestamp float64
+	data          uint64
+	nanoTimestamp int64
 }
 
-// Not const so it can be changed in tests.
-var MAX_DATA_TO_STORE = 120
-
-const NANO_TO_MILLI = float64(time.Millisecond)
+const (
+	INTERVAL_MARGIN_ERR = 0.05
+	NANO_TO_MILLI = float64(time.Millisecond)
+)
 
 const (
 	CPU_PERCENT_NAME = "cpu_percent"
@@ -83,76 +78,76 @@ var validResourceNames = map[string]bool{
 
 // Cleans data from ResourceManager.
 func (r *ResourceManager) CleanData() {
-	resourceManager.resourceHolders = []*ResourceHolder{}
+	r.resourceHolders = []*ResourceHolder{}
 }
 
 // Get the nth entry in the data.  Accepts a negaitve number, as well, so that
 // the last etc. can be referenced.
-func (r *ResourceHolder) getNthData(index int) (*DataTimestamp, error) {
+func (r *ResourceHolder) getNthData(index int64) (*DataTimestamp, error) {
 	data := r.dataTimestamps
-	dataLen := len(data)
+	dataLen := int64(len(data))
 	if index < 0 {
-		if index+dataLen < 0 {
+		if index+dataLen < int64(0) {
 			return nil, fmt.Errorf("Cannot have a negative index '%v' larger than "+
 				"the data length '%v'.", index, dataLen)
 		}
-		if dataLen == MAX_DATA_TO_STORE {
+		if dataLen == r.maxDataToStore {
 			lastIndex := r.firstEntryIndex + index
 			if lastIndex < 0 {
 				lastIndex = dataLen + lastIndex
 			}
-			return &data[lastIndex], nil
+			return data[lastIndex], nil
 		} else {
-			return &data[dataLen+index], nil
+			return data[dataLen+index], nil
 		}
 	} else {
-		return &data[index], nil
+		return data[index], nil
 	}
 	panic("Can't get here.")
 }
 
 // Given an array of data which is of type ProcUsedTimestamp, will return the
 // percent of proc time that was used.
-func (r *ResourceHolder) calculateProcPercent() (float64, error) {
-	data := r.dataTimestamps
-	dataLen := len(data)
-	if dataLen <= 1 {
-		return 0, nil
-	}
-	last, err := r.getNthData(-1)
-	if err != nil {
-		return 0, err
-	}
-	secondLast, err := r.getNthData(-2)
-	if err != nil {
-		return 0, err
-	}
-	lastProc := last.data.(float64)
-	secondLastProc := secondLast.data.(float64)
-	lastMilli := last.nanoTimestamp / NANO_TO_MILLI
-	secondLastMilli := secondLast.nanoTimestamp / NANO_TO_MILLI
-	return (lastProc - secondLastProc) / (lastMilli - secondLastMilli), nil
+func (r *ResourceHolder) calculateProcPercent(point1 *DataTimestamp,
+	point2 *DataTimestamp) (uint64, error) {
+	lastProc := float64(point1.data)
+	secondLastProc := float64(point2.data)
+	lastMilli := float64(point1.nanoTimestamp) / NANO_TO_MILLI
+	secondLastMilli := float64(point2.nanoTimestamp) / NANO_TO_MILLI
+	return uint64(100 * (lastProc - secondLastProc) / (lastMilli - secondLastMilli)), nil
 }
 
-// Given a ParsedEvent, will populate the correct resourceHolder with the
-// resource value.
-func (r *ResourceManager) gatherResource(parsedEvent *ParsedEvent,
-	pid int) error {
+// Gets a ResourceHolder, given a ParsedEvent.  Creates a new one if one doesn't
+// exist.
+func (r *ResourceManager) getResourceHolder(
+	parsedEvent *ParsedEvent) *ResourceHolder {
 	processName := parsedEvent.processName
 	resourceName := parsedEvent.resourceName
 	for _, resourceHolder := range r.resourceHolders {
 		if resourceHolder.processName == processName &&
 			resourceHolder.resourceName == resourceName {
-			if err := r.gather(pid, resourceHolder); err != nil {
-				return err
-			}
-			return nil
+			return resourceHolder
 		}
 	}
 	resourceHolder := &ResourceHolder{
 		processName:  processName,
 		resourceName: resourceName,
 	}
+	interval := float64(parsedEvent.interval.Seconds())
+	duration := float64(parsedEvent.duration.Seconds())
+	if duration == 0.0 {
+		duration = interval
+	}
+
+	resourceHolder.maxDataToStore = int64(math.Ceil(duration / interval))
+	return resourceHolder
+}
+
+// Given a ParsedEvent, will populate the correct resourceHolder with the
+// resource value.
+func (r *ResourceManager) gatherResource(parsedEvent *ParsedEvent,
+	pid int) error {
+	resourceHolder := r.getResourceHolder(parsedEvent)
 	if err := r.gather(pid, resourceHolder); err != nil {
 		return err
 	}
@@ -160,35 +155,98 @@ func (r *ResourceManager) gatherResource(parsedEvent *ParsedEvent,
 	return nil
 }
 
+// Returns the average of an array of DataTimestamps.
+func averageDataTimestampArray(array []*DataTimestamp) uint64 {
+	sum := uint64(0)
+	for _, val := range array {
+		sum += val.data
+	}
+	return sum / uint64(len(array))
+}
+
+// Gets all entries in a resource holder since a nanosecond unix timestamp.
+func (r *ResourceHolder) getEntriesSince(
+	nanosecondStart int64) []*DataTimestamp {
+	entries := []*DataTimestamp{}
+	index := int64(-1)
+	for {
+		dataTimestamp, err := r.getNthData(index)
+		index--
+		if err != nil {
+			break
+		}
+		entries = append(entries, dataTimestamp)
+		if nanosecondStart > dataTimestamp.nanoTimestamp {
+			break
+		}
+	}
+	return entries
+}
+
+// Given an array of DataTimestamp entries, and a few other parameters, this
+// function will return the average over the duration.
+func (r *ResourceHolder) getDurationData(entries []*DataTimestamp,
+	duration time.Duration, interval time.Duration,
+	resourceName string) (uint64, error) {
+	first := entries[0]
+	last := entries[len(entries)-1]
+	marginErr := 1 - INTERVAL_MARGIN_ERR
+	errDuration := int64(marginErr*float64(duration.Nanoseconds()))
+	// Add interval because if we have 3 entries that are 1s interval then we have
+	// 2s as time covered, but really it covers 3s.
+	timeDataCovers := first.nanoTimestamp-last.nanoTimestamp+
+		(interval.Nanoseconds())
+	if timeDataCovers > errDuration {
+		if resourceName == MEMORY_USED_NAME {
+			return averageDataTimestampArray(entries), nil
+		} else if resourceName == CPU_PERCENT_NAME {
+			return r.calculateProcPercent(first, last)
+		}
+	}
+	return 0, nil
+}
+
+// Gets the current data for an event.
+func (r *ResourceHolder) getData(parsedEvent *ParsedEvent) (uint64, error) {
+	resourceName := parsedEvent.resourceName
+	duration := parsedEvent.duration
+	interval := parsedEvent.interval
+	if (duration.Seconds() / interval.Seconds()) > 1 {
+		timeNow := time.Now().UnixNano()
+		entries := r.getEntriesSince(timeNow - duration.Nanoseconds())
+		if len(entries) <= 1 {
+			return 0, nil
+		}
+		return r.getDurationData(entries, duration, interval, resourceName)
+	} else {
+		// If we're not dealing with a duration.
+		data, err := r.getNthData(-1)
+		return data.data, err
+	}
+	panic("Can't get here.")
+}
+
 // Takes a ParsedEvent and pid and returns the value for the resource used in
 // the rule.
 func (r *ResourceManager) GetResource(parsedEvent *ParsedEvent,
-	pid int) (interface{}, error) {
+	pid int) (uint64, error) {
 	resourceName := parsedEvent.resourceName
 	processName := parsedEvent.processName
 	if err := r.gatherResource(parsedEvent, pid); err != nil {
-		return nil, err
+		return 0, err
 	}
 	for _, resourceHolder := range r.resourceHolders {
 		lenResourceData := len(resourceHolder.dataTimestamps)
 		if resourceHolder.processName == processName &&
 			resourceHolder.resourceName == resourceName && lenResourceData > 0 {
-			if resourceName == CPU_PERCENT_NAME {
-				procPercent, err := resourceHolder.calculateProcPercent()
-				if err != nil {
-					return nil, err
-				}
-				return procPercent, nil
-			} else if resourceName == MEMORY_USED_NAME {
-				last, err := resourceHolder.getNthData(-1)
-				if err != nil {
-					return nil, err
-				}
-				return last.data, nil
+			data, err := resourceHolder.getData(parsedEvent)
+			if err != nil {
+				return 0, err
 			}
+			return data, nil
 		}
 	}
-	return nil,
+	return 0,
 		fmt.Errorf("Could not find resource value for resource %v on process %v.",
 			resourceName, processName)
 }
@@ -198,18 +256,18 @@ func (r *ResourceManager) GetResource(parsedEvent *ParsedEvent,
 // the amount is '14mb' which this function would turn into a uint64 of
 // 14*1024*1024.
 func (r *ResourceManager) ParseAmount(resourceName string,
-	amount string) (interface{}, error) {
+	amount string) (uint64, error) {
 	if resourceName == MEMORY_USED_NAME {
 		lenAmount := len(amount)
 		if lenAmount < 3 {
-			return nil, fmt.Errorf("%v '%v' is not the correct format.",
+			return 0, fmt.Errorf("%v '%v' is not the correct format.",
 				resourceName, amount)
 		}
 		units := amount[lenAmount-2:]
 		amount = amount[0 : lenAmount-2]
 		amountUi, err := strconv.ParseUint(amount, 10, 64)
 		if err != nil {
-			return nil, err
+			return 0, err
 		}
 		switch units {
 		case "kb":
@@ -219,31 +277,31 @@ func (r *ResourceManager) ParseAmount(resourceName string,
 		case "gb":
 			amountUi *= 1024 * 1024 * 1024
 		default:
-			return nil, fmt.Errorf("Invalid units '%v' on '%v'.",
+			return 0, fmt.Errorf("Invalid units '%v' on '%v'.",
 				units, resourceName)
 		}
 		return amountUi, nil
 	} else if resourceName == CPU_PERCENT_NAME {
-		amountUi, err := strconv.ParseFloat(amount, 64)
+		amountUi, err := strconv.ParseUint(amount, 10, 64)
 		return amountUi, err
 	}
-	return nil, fmt.Errorf("Unknown resource name %v.", resourceName)
+	return 0, fmt.Errorf("Unknown resource name %v.", resourceName)
 }
 
 // Saves a data point into the data array of the ResourceHolder.
-func (r *ResourceHolder) saveData(dataToSave interface{}) {
-	dataTimestamp := DataTimestamp{
+func (r *ResourceHolder) saveData(dataToSave uint64) {
+	dataTimestamp := &DataTimestamp{
 		data:          dataToSave,
-		nanoTimestamp: float64(time.Now().UnixNano()),
+		nanoTimestamp: time.Now().UnixNano(),
 	}
-
-	if len(r.dataTimestamps) > MAX_DATA_TO_STORE {
+	timestampLen := int64(len(r.dataTimestamps))
+	if timestampLen > r.maxDataToStore {
 		panic("This shouldn't happen.")
-	} else if len(r.dataTimestamps) == MAX_DATA_TO_STORE {
+	} else if timestampLen == r.maxDataToStore {
 		// Clear out old data.
 		r.dataTimestamps[r.firstEntryIndex] = dataTimestamp
 		r.firstEntryIndex++
-		if r.firstEntryIndex == MAX_DATA_TO_STORE {
+		if r.firstEntryIndex == r.maxDataToStore {
 			r.firstEntryIndex = 0
 		}
 	} else {
--- resourcemanager_test.go
+++ resourcemanager_test.go
@@ -11,7 +11,7 @@ import (
 
 type FakeSigarGetter struct {
 	memResident uint64
-	procUsed    float64
+	procUsed    uint64
 	sysMemUsed  uint64
 }
 
@@ -20,8 +20,8 @@ func (s *FakeSigarGetter) getMemResident(pid int) (uint64, error) {
 }
 
 // Gets the proc time and a timestamp and returns a DataTimestamp.
-func (s *FakeSigarGetter) getProcTime(pid int) (float64, error) {
-	return float64(s.procUsed), nil
+func (s *FakeSigarGetter) getProcTime(pid int) (uint64, error) {
+	return s.procUsed, nil
 }
 
 var r ResourceManager
@@ -32,28 +32,30 @@ func Setup() {
 
 func TestCalculateProcPercent(t *testing.T) {
 	Setup()
-	first := DataTimestamp{
-		data: float64(2886), nanoTimestamp: float64(1.342471447022575e+18)}
-	second := DataTimestamp{
-		data: float64(3849), nanoTimestamp: float64(1.342471449022077e+18)}
-	var data []DataTimestamp
+	first := &DataTimestamp{
+		data: 2886, nanoTimestamp: 1.342471447022575e+18}
+	second := &DataTimestamp{
+		data: 3849, nanoTimestamp: 1.342471449022077e+18}
+	var data []*DataTimestamp
 	data = append(data, first)
 	data = append(data, second)
-	resourceHolder := ResourceHolder{dataTimestamps: data}
-	proc, err := resourceHolder.calculateProcPercent()
+	resourceHolder := ResourceHolder{dataTimestamps: data, maxDataToStore: 3}
+	proc, err := resourceHolder.calculateProcPercent(second, first)
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.Equal(t, 0.48161999345784273, proc)
+	assert.Equal(t, uint64(48), proc)
 }
 
 func TestGatherMem(t *testing.T) {
 	Setup()
 	duration, _ := time.ParseDuration("0s")
+	interval, _ := time.ParseDuration("1s")
 	r.SetSigarInterface(&FakeSigarGetter{memResident: 1024})
 	pe := &ParsedEvent{
 		resourceName: MEMORY_USED_NAME,
 		duration:     duration,
+		interval:     interval,
 	}
 	resourceVal, err := r.GetResource(pe, 1234)
 	if err != nil {
@@ -70,31 +72,17 @@ func TestgetMemResident(t *testing.T) {
 	assert.NotEqual(t, 0, val)
 }
 
-// Can't get proc percent with only one data point.
-func TestGatherFirstProcPercentZero(t *testing.T) {
-	Setup()
-	duration, _ := time.ParseDuration("0s")
-	r.SetSigarInterface(&FakeSigarGetter{procUsed: 2886})
-	pe := &ParsedEvent{
-		resourceName: "cpu_percent",
-		duration:     duration,
-	}
-	resourceVal, err := r.GetResource(pe, 1234)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert.Equal(t, float64(0), resourceVal)
-}
-
 // When we have gotten proc percent twice, then we can get the proc time.
 func TestGatherProcPercent(t *testing.T) {
 	Setup()
-	duration, _ := time.ParseDuration("0s")
+	duration, _ := time.ParseDuration("2s")
+	interval, _ := time.ParseDuration("1s")
 	fsg := FakeSigarGetter{procUsed: 2886}
 	r.SetSigarInterface(&fsg)
 	pe := &ParsedEvent{
 		resourceName: "cpu_percent",
 		duration:     duration,
+		interval:     interval,
 	}
 	resourceVal, err := r.GetResource(pe, 1234)
 	if err != nil {
@@ -102,11 +90,14 @@ func TestGatherProcPercent(t *testing.T) {
 	}
 	fsg.procUsed = 3849
 	r.SetSigarInterface(&fsg)
+	timeThen := r.resourceHolders[0].dataTimestamps[0].nanoTimestamp
+	// Set the time to be 1 second ago.
+	r.resourceHolders[0].dataTimestamps[0].nanoTimestamp = timeThen - 1000000000
 	resourceVal, err = r.GetResource(pe, 1234)
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.NotEqual(t, float64(0), resourceVal)
+	assert.NotEqual(t, uint64(0), resourceVal)
 }
 
 func TestParseAmountErrors(t *testing.T) {
@@ -122,7 +113,7 @@ func TestParseAmountErrors(t *testing.T) {
 	assert.Equal(t, "Invalid units 'zb' on 'memory_used'.", err.Error())
 
 	_, err = r.ParseAmount(CPU_PERCENT_NAME, "5zb")
-	assert.Equal(t, "strconv.ParseFloat: parsing \"5zb\": invalid syntax",
+	assert.Equal(t, "strconv.ParseUint: parsing \"5zb\": invalid syntax",
 		err.Error())
 }
 
@@ -134,66 +125,98 @@ func TestIsValidResourceName(t *testing.T) {
 }
 
 func TestSaveDataReusesSlice(t *testing.T) {
-	oldMax := MAX_DATA_TO_STORE
-	MAX_DATA_TO_STORE = 3
 	rh := &ResourceHolder{}
-	for i := 0; i < MAX_DATA_TO_STORE; i++ {
-		rh.saveData(i)
+	rh.maxDataToStore = 3
+	for i := int64(0); i < rh.maxDataToStore; i++ {
+		rh.saveData(uint64(i))
 	}
 	data := rh.dataTimestamps
-	assert.Equal(t, 0, data[0].data)
-	assert.Equal(t, MAX_DATA_TO_STORE-1, data[len(data)-1].data)
+	assert.Equal(t, uint64(0), data[0].data)
+	assert.Equal(t, uint64(rh.maxDataToStore-1), data[len(data)-1].data)
 	rh.saveData(1337)
-	assert.Equal(t, 1337, data[0].data)
+	assert.Equal(t, uint64(1337), data[0].data)
 	rh.saveData(1338)
-	assert.Equal(t, 1338, data[1].data)
-	MAX_DATA_TO_STORE = oldMax
+	assert.Equal(t, uint64(1338), data[1].data)
 }
 
 func TestCircularProcData(t *testing.T) {
-	oldMax := MAX_DATA_TO_STORE
-	MAX_DATA_TO_STORE = 3
+	Setup()
+	duration, _ := time.ParseDuration("3s")
+	interval, _ := time.ParseDuration("1s")
+	// 3 / 1 = 3 pieces of data max.
+	maxData := 3
 	pe := &ParsedEvent{
 		resourceName: "cpu_percent",
+		duration:     duration,
+		interval:     interval,
 	}
-	for i := 0; i < MAX_DATA_TO_STORE; i++ {
+	for i := 0; i < maxData; i++ {
 		fsg := &FakeSigarGetter{
-			procUsed: float64(2886 + i),
+			procUsed: uint64(2886 + i),
 		}
 		r.SetSigarInterface(fsg)
-		//  put in the fake sigarwrapper shit here
 		_, err := r.GetResource(pe, 1234)
 		if err != nil {
 			t.Fatal(err)
 		}
 	}
-	resourceHolders := r.resourceHolders
-	var rh *ResourceHolder
-	for _, resourceHolder := range resourceHolders {
-		if resourceHolder.resourceName == "cpu_percent" {
-			rh = resourceHolder
-			break
-		}
-	}
+	rh := r.getResourceHolder(pe)
 
 	// Assert we have what we expect.
-	assert.Equal(t, float64(2886), rh.dataTimestamps[0].data)
-	assert.Equal(t, float64(2888),
+	assert.Equal(t, uint64(2886), rh.dataTimestamps[0].data)
+	assert.Equal(t, uint64(2888),
 		rh.dataTimestamps[len(rh.dataTimestamps)-1].data)
 
 	// Now, make sure that we loop on the data slice.
 	fsg := &FakeSigarGetter{
-		procUsed: float64(2897),
+		procUsed: uint64(2897),
+	}
+	r.SetSigarInterface(fsg)
+	_, err := r.GetResource(pe, 1234)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Assert that we looped.
+	assert.Equal(t, uint64(2897), rh.dataTimestamps[0].data)
+}
+
+func TestDuration(t *testing.T) {
+	Setup()
+	duration, _ := time.ParseDuration("3s")
+	interval, _ := time.ParseDuration("1s")
+	pe := &ParsedEvent{
+		resourceName: "cpu_percent",
+		duration:     duration,
+		interval:     interval,
+	}
+	// Under 2 so we can manually add the 3rd piece.
+	for i := 0; i < 2; i++ {
+		fsg := &FakeSigarGetter{
+			procUsed: uint64(2886 + i),
+		}
+		r.SetSigarInterface(fsg)
+		_, err := r.GetResource(pe, 1234)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	rh := r.getResourceHolder(pe)
+	rh.dataTimestamps[0].nanoTimestamp =
+		rh.dataTimestamps[0].nanoTimestamp - 2000000000
+	rh.dataTimestamps[1].nanoTimestamp =
+		rh.dataTimestamps[1].nanoTimestamp - 1000000000
+
+	fsg := &FakeSigarGetter{
+		procUsed: uint64(4000),
 	}
 	r.SetSigarInterface(fsg)
 	resourceVal, err := r.GetResource(pe, 1234)
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert.Equal(t, float64(2897), rh.dataTimestamps[0].data)
-	time1 := rh.dataTimestamps[0].nanoTimestamp / NANO_TO_MILLI
-	time2 := rh.dataTimestamps[MAX_DATA_TO_STORE-1].nanoTimestamp / NANO_TO_MILLI
-	expected := float64(2897-2888) / (time1 - time2)
-	assert.Equal(t, expected, resourceVal)
-	MAX_DATA_TO_STORE = oldMax
+	timeDiff := float64(rh.dataTimestamps[2].nanoTimestamp -
+		rh.dataTimestamps[0].nanoTimestamp) / NANO_TO_MILLI
+	valDiff := rh.dataTimestamps[2].data - rh.dataTimestamps[0].data
+	expectedPercent := uint64(100 * (float64(valDiff) / float64(timeDiff)))
+	assert.Equal(t, expectedPercent, resourceVal)
 }
