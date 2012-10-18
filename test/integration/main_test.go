84bf2f658312e6cd65dbb17ffe156db5a89e728d
Ben Lisbakken <lisbakke@gmail.com>
2012-10-18 16:06:34 -0600

    Initial integration tests.
    Adds helper functions to start/stop gonit, run gonit commands and
    assert the existence of processes and log messages.
    
    Change-Id: I54857ace9fef23973d2cec3aaa675b1f5611e122

---
+++ test/integration/main_test.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2012 VMware, Inc.
+
+package gonit_integration
+
+import (
+	"flag"
+	"github.com/cloudfoundry/gonit/test/helper"
+	. "launchpad.net/gocheck"
+	"log"
+	"os"
+	"testing"
+)
+
+var (
+	integrationDir    string
+	gonitMainDir      string
+	integrationTmpDir string
+)
+
+func setup() {
+	flag.Parse()
+
+	if intDir, err := os.Getwd(); err != nil {
+		log.Fatalf("Error getting pwd: %v", err.Error())
+	} else {
+		integrationDir = intDir
+	}
+
+	integrationTmpDir := integrationDir + "/tmp"
+	gonitMainDir = integrationDir + "/../../gonit"
+
+	if err := os.MkdirAll(integrationTmpDir, 0755); err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err := helper.BuildBin(integrationDir+"/../process",
+		integrationTmpDir+"/goprocess")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err = helper.BuildBin(gonitMainDir, integrationTmpDir+"/gonit")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	if err := os.Chdir(integrationTmpDir); err != nil {
+		log.Fatalf("Error changing dir: %v", err.Error())
+	}
+}
+
+func cleanup() {
+	if err := os.RemoveAll(integrationDir + "/tmp"); err != nil {
+		log.Printf("Error deleting '%v': '%v'", integrationTmpDir, err.Error())
+	}
+}
+
+func Test(t *testing.T) {
+	setup()
+	TestingT(t)
+	cleanup()
+}
