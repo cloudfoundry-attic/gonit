24eacf36a1b7ab8fb482168c6e89d72dfb5d333e
Ben Lisbakken <lisbakke@gmail.com>
2012-10-18 16:06:34 -0600

    Initial integration tests.
    Adds helper functions to start/stop gonit, run gonit commands and
    assert the existence of processes and log messages.
    
    Change-Id: I54857ace9fef23973d2cec3aaa675b1f5611e122

---
+++ test/integration/eventmonitor_test.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2012 VMware, Inc.
+
+package gonit_integration
+
+import (
+	"github.com/cloudfoundry/gonit/test/helper"
+	"io"
+	. "launchpad.net/gocheck"
+	"os/exec"
+)
+
+type EventIntSuite struct {
+	gonitCmd     *exec.Cmd
+	expectedExit *bool
+	stdout       io.ReadCloser
+}
+
+var _ = Suite(&EventIntSuite{})
+
+var balloonPidFile = "../test/integration/balloonmem.pid"
+
+func (s *EventIntSuite) SetUpTest(c *C) {
+	intCfgDir := integrationDir + "/../config/eventmonitor_integration"
+	s.gonitCmd, s.stdout, s.expectedExit = helper.StartGonit(intCfgDir, verbose)
+}
+
+func (s *EventIntSuite) TearDownTest(c *C) {
+	helper.StopGonit(s.gonitCmd, verbose, s.expectedExit)
+	helper.RemoveFilesWithExtension(integrationDir, ".pid")
+	helper.RemoveFilesWithExtension(integrationDir+"/..", ".json")
+}
+
+func (s *EventIntSuite) TestAlertRule(c *C) {
+	helper.RunGonitCmd("start all", verbose)
+	c.Check(true, Equals, helper.FindLogLine(s.stdout,
+		"'balloonmem' triggered 'memory_used > 1mb' for '1s'", "5s"))
+}
+
+func (s *EventIntSuite) TestRestartRule(c *C) {
+	helper.RunGonitCmd("start all", verbose)
+
+	balloonPid, err := helper.ProxyReadPidFile(balloonPidFile)
+	c.Check(err, IsNil)
+	helper.AssertProcessExists(c, balloonPid)
+	pid1, _ := helper.ProxyReadPidFile(balloonPidFile)
+	c.Check(true, Equals,
+		helper.FindLogLine(s.stdout, "Executing 'restart'", "20s"))
+	c.Check(true, Equals,
+		helper.FindLogLine(s.stdout, "process \"balloonmem\" started", "10s"))
+	balloonPid, err = helper.ProxyReadPidFile(balloonPidFile)
+	c.Check(err, IsNil)
+	helper.AssertProcessExists(c, balloonPid)
+	pid2, _ := helper.ProxyReadPidFile(balloonPidFile)
+	c.Check(pid1, Not(Equals), pid2)
+}
---
+++ test/integration/main_test.go
@@ -0,0 +1,36 @@
+// Copyright (c) 2012 VMware, Inc.
+
+package gonit_integration
+
+import (
+	"flag"
+	"fmt"
+	"github.com/cloudfoundry/gonit/test/helper"
+	. "launchpad.net/gocheck"
+	"os"
+	"testing"
+)
+
+var (
+	verbose        = false
+	integrationDir string
+	gonitMainDir   string
+)
+
+func Test(t *testing.T) {
+	if flag.Lookup("gocheck.vv").Value.String() == "true" {
+		verbose = true
+	}
+	flag.Parse()
+	if intDir, err := os.Getwd(); err != nil {
+		fmt.Printf("Error getting pwd: %v", err.Error())
+		os.Exit(2)
+	} else {
+		integrationDir = intDir
+	}
+	gonitMainDir = integrationDir + "/../../gonit"
+	helper.BuildBin(integrationDir+"/../process", integrationDir+"/goprocess")
+	helper.BuildBin(gonitMainDir, "")
+	helper.ChDirOrExit(gonitMainDir)
+	TestingT(t)
+}
